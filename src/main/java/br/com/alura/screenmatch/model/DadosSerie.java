e78839884f0a275a93047a8ce9ea43d32ca333dc
Gleibson Oliveira <gleibson.rodrigo>
2023-08-26 17:05:38 -0300

    feat: criando record DadosSerie e associando ao retorno json da API

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -1,7 +1,9 @@
 package br.com.alura.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true) //ignorando propriedades não mapeadas
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
                          @JsonAlias("imdbRating") String avaliacao) {
