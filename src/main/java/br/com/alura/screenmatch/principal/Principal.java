59ccdc184fd6e258216b63cae310c0bea147a87a
Gleibson Oliveira <gleibson.rodrigo>
2023-08-27 19:43:15 -0300

    feat: construindo estatística com gruopingBy e averagingDouble com filter

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -111,6 +111,9 @@ public class Principal {
                 ));
 
 
-
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada, Collectors.averagingDouble(Episodio::getAvaliacao)));
+        System.out.println(avaliacoesPorTemporada);
     }
 }
